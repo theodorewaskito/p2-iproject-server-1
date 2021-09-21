4beda3f1dd23802b52ca2dda9caa96a6dbb59acf
theodorewaskito <theodorewaskito@gmail.com>
2021-09-21 18:33:28 +0700

    penambahan routes

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const cors = require('cors')
+const routes = require("./routes/index")
+
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(routes)
+
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
