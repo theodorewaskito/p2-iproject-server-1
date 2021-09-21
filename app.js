9ce4de17f974d008d624d95844bc5ef27f0280b8
theodorewaskito <theodorewaskito@gmail.com>
2021-09-21 18:19:19 +0700

    penambahan app.use

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
