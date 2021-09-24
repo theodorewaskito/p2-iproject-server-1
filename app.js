03a7557336a335135539e87faa237ee3468d0c0c
theodorewaskito <theodorewaskito@gmail.com>
2021-09-24 10:50:45 +0700

    perubahan port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ if(process.env.NODE_ENV !== "production") {
 }
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const cors = require('cors')
 const routes = require("./routes/index")
 
